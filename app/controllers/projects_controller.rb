d8e01905b9c57f111cf48a385c50fe92f0535ace
Dustin McBride <dustin@chivve.com>
2013-02-01 14:38:33 -0600

    added some ghetto code for demo

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -14,14 +14,16 @@ end
 
 def index
     
-    @projects = @agency.projects.all
+    @projects = @agency.projects.find(:all, :offset => 40, :limit => 10)
+
+
 
   end
 
    def show
 
     @project = Project.find(params[:id])
-    
+
 
     #@subnav = [["Back", agency_path(@agency), "btn back"], ["Edit Details", edit_agency_project_path(@agency)], ["Edit Images", project_images_path(@project)], ["Add Image", new_project_image_path(@project)], ['Add Question', new_project_question_path(@project)], ['Edit Question', project_questions_path(@project)]]
     #@right_subnav = [['Publish', '#publish',"","modal" ],['Archive', '#archive',"","modal" ],['Delete', '#delete',"","modal" ]]
